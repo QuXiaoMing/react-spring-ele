e9bb79524b84c590618be29a5bee4552edaf6c1b
quxiaoming <quxiaoming@szzt.com.cn>
2019-06-24 15:11:54 +0800

    【初始化】

---
+++ src/main/java/com/shellming/eleservice/EleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shellming.eleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleServiceApplication.class, args);
+	}
+
+}
