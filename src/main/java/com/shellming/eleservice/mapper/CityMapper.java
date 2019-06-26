ae9ac88ea89fa1a29472085bbc32996ce5bc9f3f
quxiaoming <quxiaoming@szzt.com.cn>
2019-06-26 19:14:45 +0800

    【增加】增加City

---
+++ src/main/java/com/shellming/eleservice/mapper/CityMapper.java
@@ -0,0 +1,17 @@
+package com.shellming.eleservice.mapper;
+
+import com.shellming.eleservice.entity.City;
+
+public interface CityMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(City record);
+
+    int insertSelective(City record);
+
+    City selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(City record);
+
+    int updateByPrimaryKey(City record);
+}
