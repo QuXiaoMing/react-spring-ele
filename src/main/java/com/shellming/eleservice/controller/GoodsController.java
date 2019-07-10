db4e616406cd6a1fe595dba48ef2cf565772174d
QuXiaoMing <xm726@qq.com>
2019-07-10 20:53:26 +0800

    【修改】商品详情接口

--- src/main/java/com/shellming/eleservice/controller/GoodsController.java
+++ src/main/java/com/shellming/eleservice/controller/GoodsController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
-@Api(value = "商品接口", description ="商品接口")
+@Api(value = "商品接口", description = "商品接口")
 @RestController
 @RequestMapping(value = "goods")
 @Slf4j
@@ -24,7 +24,7 @@ public class GoodsController {
 
     @ApiOperation(value = "商品列表")
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResultBean list (@RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "1") int pageNum) throws Exception {
+    public ResultBean list(@RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "1") int pageNum) throws Exception {
         HashMap map = new HashMap();
         PageHelper.startPage(pageNum, pageSize);
         log.info("商品列表{}", map);
@@ -35,7 +35,7 @@ public class GoodsController {
 
     @ApiOperation(value = "添加商品")
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResultBean insert (@RequestBody Goods goods) {
+    public ResultBean insert(@RequestBody Goods goods) {
         log.info("添加商品{}", goods);
         int ret = goodsService.insert(goods);
         if (ret > 0) {
@@ -46,7 +46,7 @@ public class GoodsController {
 
     @ApiOperation(value = "修改商品信息")
     @RequestMapping(value = "", method = RequestMethod.PUT)
-    public ResultBean updateByPrimaryKeySelective (@RequestBody Goods goods) {
+    public ResultBean updateByPrimaryKeySelective(@RequestBody Goods goods) {
         log.info("修改商品商品{}", goods);
         int ret = goodsService.updateByPrimaryKeySelective(goods);
         if (ret > 0) {
@@ -64,5 +64,14 @@ public class GoodsController {
             return ResultBean.success(ret);
         }
         return ResultBean.fail("删除失败");
+    }
+
+    @ApiOperation(value = "获取商品详情")
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public ResultBean selectByPrimaryKey(@PathVariable("id") int id) {
+        log.info("获取商品详情{}", id);
+        Goods ret = goodsService.selectByPrimaryKey(id);
+        log.info("获取商品详情{}", ret);
+        return ResultBean.success(ret);
     }
 }
