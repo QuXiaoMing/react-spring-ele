0bf74ee24ae501d4ddfbd15c2ea274beac4295d2
QuXiaoMing <xm726@qq.com>
2019-07-12 10:10:22 +0800

    【修改】增加文件上传接口

--- src/main/java/com/shellming/eleservice/controller/FileUploadController.java
+++ src/main/java/com/shellming/eleservice/controller/FileUploadController.java
@@ -1,13 +1,11 @@
 package com.shellming.eleservice.controller;
 
 
-import com.alibaba.fastjson.JSON;
 import com.shellming.eleservice.common.ResultBean;
 import com.shellming.eleservice.service.impl.FileUploadServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "文件上传", description = "文件上传")
 @RequestMapping("/upload")
---
+++ src/main/java/com/shellming/eleservice/vo/UserVo.java
@@ -0,0 +1,10 @@
+package com.shellming.eleservice.vo;
+
+import com.shellming.eleservice.entity.User;
+
+public class UserVo extends User {
+    @Override
+    public String getPassword() {
+        return null;
+    }
+}
